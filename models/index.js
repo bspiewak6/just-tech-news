327f75e56d8dd45bb3a9ea585dc4b550260d12c1
Spiewak <bspiewak6@gmail.com>
2020-10-28 11:02:48 -0400

    updated post routes, CRUD and order property

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations 
+// 1 to many relationship
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// each post has a 1 to 1 relationship with user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
