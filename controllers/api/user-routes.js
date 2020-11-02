4038d4ee73f07cced95f97ca69f1487f51d90624
Spiewak <bspiewak6@gmail.com>
2020-11-02 12:42:01 -0500

    updated logout, reset cookies and destroy sessions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -103,11 +103,11 @@ router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
-  });
-}
-else {
-  res.status(404).end()
-}
+    });
+  }
+  else {
+    res.status(404).end()
+  }
 });
 
 router.put('/:id', (req, res) => {
